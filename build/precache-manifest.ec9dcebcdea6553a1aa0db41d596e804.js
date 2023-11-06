0d991d76e9ad50e48021567a94bfccfe2f1752e3
root <root@ip-172-31-16-150.ec2.internal>
2023-11-06 14:33:25 +0000

    aa

---
+++ build/precache-manifest.ec9dcebcdea6553a1aa0db41d596e804.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84fcafd83145d397f7170fce0255e515",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffe4ad06d822818da56b",
+    "url": "/static/css/main.43b6b070.chunk.css"
+  },
+  {
+    "revision": "a82c789e4a99d29bd732",
+    "url": "/static/js/2.57cf4e78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57cf4e78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe4ad06d822818da56b",
+    "url": "/static/js/main.af7605fe.chunk.js"
+  },
+  {
+    "revision": "c27e8218b895e978a16c",
+    "url": "/static/js/runtime-main.ef6f086a.js"
+  }
+]);
